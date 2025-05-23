5fafc7872a5a8565d6ea17dd00b9b3762b627a1c
Shivank2401 <138654600+Shivank2401@users.noreply.github.com>
2025-05-23 16:43:07 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,7 +16,7 @@ def run():
     # Convert date column upfront
     df['Date & Time Added'] = pd.to_datetime(df['Date & Time Added'], errors='coerce')
 
-    def metric_box(label, value, bg_color, text_color="white", width="300px"):
+    def metric_box(label, value, bg_color, text_color="white", width="250px"):
         box_html = f"""
         <div style="
             background-color: {bg_color};
